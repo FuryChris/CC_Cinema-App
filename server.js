50ad623339ff5ebb13bee08ec28a2a152585ba18
apyd <arturpydych@gmail.com>
2020-01-04 09:55:44 +0100

    Set up connection to mongoDB
    Set up connection to mongoDB
    Added missing packages

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-// set server
+const helmet = require('helmet');
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(helmet());
+
+mongoose.connect(process.env.DB_CONNECT, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(console.log('Connected sucessfully to MongoDB...'))
+    .catch(err => console.log(err));
+
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
+
+module.exports = app;
