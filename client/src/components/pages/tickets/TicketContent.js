88eda88128b712fe8e8ce59abd6cc9fe82d469d5
krzysiek2222 <desmont2222@gmail.com>
2020-01-05 16:31:31 +0100

    Added state to ticket & styling fix

--- client/src/components/pages/tickets/TicketContent.js
+++ client/src/components/pages/tickets/TicketContent.js
@@ -3,50 +3,60 @@ import './Ticket.scss'
 
 
 class TicketContent extends React.Component {
+  
+  state = {
+    userName: 'John Smith', // <-- id użytkownika
+    seat: '22', // <-- zarezerwowane siedzenia, można wrzucić do potwierdzenia rezerwacji
+    movieTitle: 'Star Wars: Episode IX - The Rise of Skywalker',
+    row: 'B',
+    hall: 'C',
+    date: '12.05.2020 18:00'
+}
+
 
       render() {
                   return (   
                     <div id="cardWrap">
                     <div className="cardMain cardLeft">
-                      <h1>Startup <span>Cinema</span></h1>
+                      <h1>Babylon <span>Cinema</span></h1>
                       <div className="title">
-                        <h2>Star Wars: Episode IX - The Rise of Skywalker</h2>
+                        <h2>{this.state.movieTitle}</h2>
                         <span>movie</span>
                       </div>
                       <div className="name">
-                        <h2>John Johnovsky</h2>
+                        <h2>{this.state.userName}</h2>
                         <span>name</span>
                       </div>
                       <div className="seat">
-                        <h2>156</h2>
+                        <h2>{this.state.seat}</h2>
                         <span>seat</span>
                       </div>
             
                       <div className="row">
-                        <h2>2</h2>
+                        <h2>{this.state.row}</h2>
                         <span>row</span>
                       </div>
             
                       <div className="row">
-                        <h2>C</h2>
+                        <h2>{this.state.hall}</h2>
                         <span>hall</span>
                       </div>  
                 
                       <div className="time">
-                        <h2>12:15</h2>
-                        <span>time</span>
+                        <h2>{this.state.date}</h2>
+                        <span>date</span>
                       </div>
                     </div>
                     <div className="cardMain cardRight">
                       <div className ="number" id="hall">
-                        <h3>Hall: C</h3>
+                        <h3>Hall: {this.state.hall}</h3>
                       </div>
                       <div className="number">
-                        <h3>156</h3>
+                        <h3>{this.state.seat}</h3>
                         <span>seat</span>
                       </div>
                       <div className ="number" id="rowcut">
-                        <h3>2</h3>
+                        <h3>{this.state.row}</h3>
                         <span>row</span>
                       </div>
                       <div className="barcode"></div>
